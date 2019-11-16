addc76ccfaa1e55f9b959ca76d23c5506d7d98f5
Yasen Alexiev <yassen4o222@abv.bg>
2019-11-16 21:29:39 +0200

    fix whitespace

--- Practices/Practice 05/task11.cpp
+++ Practices/Practice 05/task11.cpp
@@ -7,7 +7,7 @@ int main() {
 	std::cin >> n;
 
 	for(int i = 1; i <= n; ++i) {
-		std::cout << "The number " << i << "cubed is: " << pow(i, 3) << std::endl;
+		std::cout << "The number " << i << " cubed is: " << pow(i, 3) << std::endl;
 	}
 
 	return 0;
