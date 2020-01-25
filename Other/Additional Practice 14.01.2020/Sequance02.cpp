d9de553253f5d33b50e9879f1c358ff4c8d37dfc
Yasen Alexiev <yassen4o222@abv.bg>
2020-01-25 17:41:24 +0200

    changeXY solution

--- Other/Additional Practice 14.01.2020/Sequance02.cpp
+++ Other/Additional Practice 14.01.2020/Sequance02.cpp
@@ -16,14 +16,33 @@ int countHi(char* text, int count = 0) {
 
 }
 
+//task 3
+void changeXY(char* source, char* destination) {
+	if(!*source) {
+		*destination = '\0';
+		return;
+	}
+
+	if(*source == 'x') {
+		*destination = 'y';
+	} else {
+		*destination = *source;
+	}
+	changeXY(source+1, destination+1);
+}
 
 int main() {
 
 	char str[MAX_SIZE];
+	char dest[MAX_SIZE];
 
 	std::cin >> str;
 
 	std::cout << countHi(str) << std::endl;
 	
+	changeXY(str, dest);
+
+	std::cout << dest << std::endl;
+
 	return 0;
 }
