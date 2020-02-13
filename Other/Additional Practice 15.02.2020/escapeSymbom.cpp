4f11e6f6d62adf1ce524d01acb2e8e997dd06b18
Yasen Alexiev <yassen4o222@abv.bg>
2020-02-13 22:48:06 +0200

    commit exaples for escape symbol and how to use it

---
+++ Other/Additional Practice 15.02.2020/escapeSymbom.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "Simple sentence with \" in it! " << std::endl;
+	std::cout << "Simple sentence with \a in it! " << std::endl;
+	std::cout << "Simple sentence with \aa in it! " << std::endl;
+
+	std::cout << "Simple sentence with \n new line! " << std::endl;
+	std::cout << "Simple sentence with \t with tab! " << std::endl;
+	std::cout << "Simple sentence with \r with carriage return! " << std::endl;
+
+	std::cout << "Simple sentence with \ in it! " << std::endl;
+	std::cout << "Simple sentence with \\ in it! " << std::endl;
+
+	return 0;
+}
